556be1d9c718d0f8343d6ddad9fb539ea42ef3f1
Arron Mabrey <arron@mabreys.com>
2021-12-21 01:45:55 -0700

    fix typo

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export NVM_DIR="$HOME/.config/nvm"
-[ -s "$NVM_DIR/nsv.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 npm --prefix ~/mabreys-chat start
